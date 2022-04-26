26bfe1ec52fd44a80a5aacca0b2ff0ced488081b
Dân Thế Nguyễn <dan.nguyen200206@vnuk.edu.vn>
2022-04-27 00:44:54 +0700

    fixed some camera bugs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
-    
-    void Start()
-    {
-        
-    }
+    public float smoothing;
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
 
     // Update is called once per frame
-    void Update()
-    {
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+    void LateUpdate() {
+        if (transform.position != target.position) {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            
+            // targetPosition.x = Mathf.Clamp(transform.position.x, minPosition.x, maxPosition.x);
+            // targetPosition.y = Mathf.Clamp(transform.position.y, minPosition.y, maxPosition.y);
+            
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
     }
 }
