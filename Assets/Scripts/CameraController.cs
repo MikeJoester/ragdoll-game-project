a83453c96d041a76786750a19c15205f42ecb9cc
Dân Thế Nguyễn <dan.nguyen200206@vnuk.edu.vn>
2022-04-27 10:15:03 +0700

    Added park's map and fixed more camera controls

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -14,8 +14,8 @@ public class CameraController : MonoBehaviour
         if (transform.position != target.position) {
             Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
             
-            // targetPosition.x = Mathf.Clamp(transform.position.x, minPosition.x, maxPosition.x);
-            // targetPosition.y = Mathf.Clamp(transform.position.y, minPosition.y, maxPosition.y);
+            targetPosition.x = Mathf.Clamp(target.position.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(target.position.y, minPosition.y, maxPosition.y);
             
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
         }
