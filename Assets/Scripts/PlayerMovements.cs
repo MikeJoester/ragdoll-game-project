b220a2f78a6dd58b225b29f6d86994d96394e106
Dân Thế Nguyễn <dan.nguyen200206@vnuk.edu.vn>
2022-04-27 13:55:38 +0700

    fixing player's movement bug

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -18,6 +18,11 @@ public class PlayerMovements : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (movement.x == 1 || movement.x == -1 || movement.y == 1 || movement.y == -1) {
+            animator.SetFloat("LastX", movement.x);
+            animator.SetFloat("LastY", movement.y);
+        }
     }
 
     void FixedUpdate() {
